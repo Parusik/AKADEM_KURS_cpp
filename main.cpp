62df22620d3b225cd0810f1afc3a495f4a49d210
Parusik <kparusik@gmail.com>
2016-09-20 14:16:34 +0300

    SFML уоки
    Підговка

---
+++ main.cpp
@@ -0,0 +1,141 @@
+#include <SFML/Graphics.hpp>
+#include "map.h"
+#include "view.h"
+using namespace sf;
+////////////////////////////////////////////////////КЛАСС ИГРОКА////////////////////////
+class Player {
+private:float x, y;
+public:
+	float w, h, dx, dy, speed = 0; //координаты игрока х и у, высота ширина, ускорение (по х и по у), сама скорость
+	int dir = 0;//направление (direction) движения игрока
+	String File;//файл с расширением
+	Image image;//сфмл изображение
+	Texture texture;
+	Sprite sprite;
+	Player(String F, int X, int Y, float W, float H) {//Конструктор с параметрами(формальными) для класса Player. При создании объекта класса мы будем задавать имя файла, координату Х и У, ширину и высоту
+		File = F;
+		w = W; h = H;
+		image.loadFromFile("images/" + File);
+		image.createMaskFromColor(Color(41, 33, 59));
+		texture.loadFromImage(image);
+		sprite.setTexture(texture);
+		x = X; y = Y;
+		sprite.setTextureRect(IntRect(w, h, w, h));
+	}
+	void udate(float time) {
+		switch (dir)
+		{
+		case 0: dx = speed; dy = 0; break;
+		case 1: dx = -speed; dy = 0; break;
+		case 2: dx = 0; dy = speed; break;
+		case 3: dx = 0; dy = -speed; break;
+		}
+		x += dx*time;
+		y += dy*time;
+		speed = 0;
+		sprite.setPosition(x, y);
+	}
+	float getplayercoordinateX() {	//этим методом будем забирать координату Х	
+		return x;
+	}
+	float getplayercoordinateY() {	//этим методом будем забирать координату Y 	
+		return y;
+	}
+};///////////////////////////////////////////////////////////////////
+
+
+int main()
+{
+	RenderWindow window(sf::VideoMode(640, 480), "GAME"); 
+	view.reset(FloatRect(0, 0, 540, 480));
+
+	Image map_image;//объект изображения для карты
+	map_image.loadFromFile("images/map.png");//загружаем файл для карты
+	Texture map;//текстура карты
+	map.loadFromImage(map_image);//заряжаем текстуру картинкой
+	Sprite s_map;//создаём спрайт для карты
+	s_map.setTexture(map);//заливаем текстуру спрайтом
+	//herotexture.loadFromFile("images/hero.png");
+	//Sprite herosprite;
+	//herosprite.setTexture(herotexture);
+	//herosprite.setTextureRect(IntRect(0,192,96, 96));
+	//herosprite.setPosition(0,0);
+
+
+	Player p("hero.png", 250, 250, 96.0, 96.0);//создаем объект p класса player,задаем "hero.png" как имя файла+расширение, далее координата Х,У, ширина, высота.
+
+	float CurrentFrame=0;
+	Clock clock; 
+
+	while (window.isOpen())
+	{
+		float time = clock.getElapsedTime().asMicroseconds();
+		clock.restart();
+		time = time / 800;
+
+		Event event;
+		while (window.pollEvent(event))
+		{
+			
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		///////////////////////////////////////////Управление персонажем с анимацией////////////////////////////////////////////////////////////////////////
+		if ((Keyboard::isKeyPressed(Keyboard::Left) || (Keyboard::isKeyPressed(Keyboard::A)))) {
+			p.dir = 1; p.speed = 0.1;//dir =1 - направление вверх, speed =0.1 - скорость движения. Заметьте - время мы уже здесь ни на что не умножаем и нигде не используем каждый раз
+			CurrentFrame += 0.005*time;
+			if (CurrentFrame > 3) CurrentFrame -= 3;
+			p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 96, 96, 96)); //через объект p класса player меняем спрайт, делая анимацию (используя оператор точку)
+			getplayercoordinateforview(p.getplayercoordinateX(), p.getplayercoordinateY());
+		}
+
+		if ((Keyboard::isKeyPressed(Keyboard::Right) || (Keyboard::isKeyPressed(Keyboard::D)))) {
+			p.dir = 0; p.speed = 0.1;//направление вправо, см выше
+			CurrentFrame += 0.005*time;
+			if (CurrentFrame > 3) CurrentFrame -= 3;
+			p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 192, 96, 96)); //через объект p класса player меняем спрайт, делая анимацию (используя оператор точку)
+			getplayercoordinateforview(p.getplayercoordinateX(), p.getplayercoordinateY());
+
+		}
+
+		if ((Keyboard::isKeyPressed(Keyboard::Up) || (Keyboard::isKeyPressed(Keyboard::W)))) {
+			p.dir = 3; p.speed = 0.1;//направление вниз, см выше
+			CurrentFrame += 0.005*time;
+			if (CurrentFrame > 3) CurrentFrame -= 3;
+			p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 288, 96, 96)); //через объект p класса player меняем спрайт, делая анимацию (используя оператор точку)
+			getplayercoordinateforview(p.getplayercoordinateX(), p.getplayercoordinateY());
+
+		}
+		if ((Keyboard::isKeyPressed(Keyboard::Down) || (Keyboard::isKeyPressed(Keyboard::S)))) { //если нажата клавиша стрелка влево или англ буква А
+			p.dir = 2; p.speed = 0.1;//направление вверх, см выше
+			CurrentFrame += 0.005*time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
+			if (CurrentFrame > 3) CurrentFrame -= 3; //проходимся по кадрам с первого по третий включительно. если пришли к третьему кадру - откидываемся назад.
+			p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 0, 96, 96)); //проходимся по координатам Х. получается 96,96*2,96*3 и опять 96
+			getplayercoordinateforview(p.getplayercoordinateX(), p.getplayercoordinateY());
+
+		}
+
+		p.udate(time);
+		window.setView(view);
+		window.clear();
+
+			/////////////////////////////Рисуем карту/////////////////////
+			for (int i = 0; i < HEIGHT_MAP; i++)
+				for (int j = 0; j < WIDTH_MAP; j++)
+				{
+					if (TileMap[i][j] == ' ')  s_map.setTextureRect(IntRect(0, 0, 32, 32)); //если встретили символ пробел, то рисуем 1й квадратик
+					if (TileMap[i][j] == 's')  s_map.setTextureRect(IntRect(32, 0, 32, 32));//если встретили символ s, то рисуем 2й квадратик
+					if ((TileMap[i][j] == '0')) s_map.setTextureRect(IntRect(64, 0, 32, 32));//если встретили символ 0, то рисуем 3й квадратик
+
+
+					s_map.setPosition(j * 32, i * 32);//по сути раскидывает квадратики, превращая в карту. то есть задает каждому из них позицию. если убрать, то вся карта нарисуется в одном квадрате 32*32 и мы увидим один квадрат
+
+					window.draw(s_map);//рисуем квадратики на экран
+				}
+
+
+		window.draw(p.sprite);
+		window.display();
+	}
+	return 0;
+}
